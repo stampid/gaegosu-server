26d0c40dce01c60d58949c0924da85e055e28d4b
stampid <vashstampid03@gmail.com>
2019-07-31 16:35:51 +0900

    test graphql write

--- src/api/test/test.js
+++ src/api/test/test.js
@@ -1,17 +1,33 @@
-import { Pet, User } from "../../../models/index";
+import { Pet, User, Board } from "../../../models/index";
 
 export default {
   Query: {
-    sayHello: () => "hi!",
-    pet: async (_, args) => {
+    testsayHello: () => "hi!",
+    testpet: async (_, args) => {
       const { id } = args;
       const pet = await Pet.findOne({ where: { id } });
       return pet;
     },
-    user: async (_, args) => {
+    testuser: async (_, args) => {
       const { id } = args;
       const user = await User.findOne({ where: { id } });
       return user;
+    }
+  },
+  Mutation: {
+    testcreateBoard: async (_, args) => {
+      const { creator, title, content, photo, boardName } = args;
+      return Board.create({
+        creator,
+        title,
+        content,
+        photo,
+        boardName
+      })
+        .then(board => {
+          return board;
+        })
+        .catch(err => console.log(err));
     }
   }
 };
