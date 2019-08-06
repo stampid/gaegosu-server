52358322e135d6b62b8f248b32497a90506cf945
stampid <vashstampid03@gmail.com>
2019-08-06 15:02:49 +0900

    change profileImg Complete

---
+++ src/api/user/myProfile/changeImage/changeImage.js
@@ -0,0 +1,43 @@
+import { User } from "../../../../../models/index";
+
+export default {
+  Mutationg: {
+    changeImage: (_, __, { req }) => {
+      const { userinfo } = req;
+      if (userinfo !== undefined && userinfo.id !== undefined) {
+        const { file } = req;
+        const { id, nickName } = userinfo;
+        if (file.Location !== undefined) {
+          const profileImage = file.Location;
+          return User.update({ profileImage }, { where: { id, nickName } })
+            .then(_ => ({
+              success: true,
+              err: null,
+              isLogin: true,
+              profileImage
+            }))
+            .catch(err => ({
+              success: false,
+              err,
+              isLogin: true,
+              profileImage: null
+            }));
+        }
+
+        return {
+          success: false,
+          err: "can't find file",
+          isLogin: true,
+          profileImage: null
+        };
+      }
+
+      return {
+        success: false,
+        err: "token expire",
+        isLogin: false,
+        profileImage: null
+      };
+    }
+  }
+};
