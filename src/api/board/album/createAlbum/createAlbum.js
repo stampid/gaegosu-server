ef6fdf9135e3a86067e2fb4353ff573ea195fe4d
stampid <vashstampid03@gmail.com>
2019-08-09 15:07:13 +0900

    album permission Check and create Album fix

--- src/api/board/album/createAlbum/createAlbum.js
+++ src/api/board/album/createAlbum/createAlbum.js
@@ -5,18 +5,14 @@ export default {
     createAlbum: async (_, args, { req }) => {
       const { userinfo } = req;
       if (userinfo !== undefined && userinfo.id !== undefined) {
-        const { title, content, creator, boardName } = args;
-        let location =
-          "https://d2x5ku95bkycr3.cloudfront.net/App_Themes/Common/images/profile/0_200.png";
-        if (req.file !== undefined) {
-          location = req.file.Location;
-        }
+        const { title, content, boardName, photo } = args;
+
         return Board.create({
-          creator,
+          creator: userinfo.id,
           title,
           content,
           boardName,
-          photo: location
+          photo
         })
           .then(board => {
             return {
