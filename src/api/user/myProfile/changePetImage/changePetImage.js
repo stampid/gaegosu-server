69b5a7b9200cbee5907568c8a36e518d445a20a4
stampid <vashstampid03@gmail.com>
2019-08-11 13:17:29 +0900

    pet profile Image change fix

--- src/api/user/myProfile/changePetImage/changePetImage.js
+++ src/api/user/myProfile/changePetImage/changePetImage.js
@@ -6,42 +6,28 @@ export default {
       const { userinfo } = req;
 
       if (userinfo !== undefined && userinfo.id !== undefined) {
-        const { file } = req;
-        const { id } = args;
+        const { id, profileImage } = args;
 
-        if (file !== undefined && file.Location !== undefined) {
-          const profileImage = file.Location;
-          return Pet.update(
-            { profileImage },
-            { where: { id, owner: userinfo.id } }
-          )
-            .then(_ => ({
-              success: true,
-              err: null,
-              isLogin: true,
-              profileImage
-            }))
-            .catch(err => ({
-              success: false,
-              err,
-              isLogin: true,
-              profileImage: null
-            }));
-        }
-
-        return {
-          success: false,
-          err: "can't find Image",
-          isLogin: true,
-          profileImage: null
-        };
+        return Pet.update(
+          { profileImage },
+          { where: { id, owner: userinfo.id } }
+        )
+          .then(_ => ({
+            success: true,
+            err: null,
+            isLogin: true
+          }))
+          .catch(err => ({
+            success: false,
+            err,
+            isLogin: true
+          }));
       }
 
       return {
         success: false,
         err: "token expire",
-        isLogin: false,
-        profileImage: null
+        isLogin: false
       };
     }
   }
